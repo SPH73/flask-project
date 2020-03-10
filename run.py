70112631e5b6f96cb7cc6c847e605471d75ab2e4
Sue Holder <susanpholder@gmail.com>
2020-03-10 15:11:29 +0000

    update contact-form form-fields with name attibute and added request module and methods to contact view in run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 
@@ -32,8 +32,10 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form)
     return render_template("contact.html", page_title="Contact")
 
 
